18448e6a0337a56e47ad643f76c39864785cb467
Aldo Santos <aldo.santosl@gmail.com>
2015-06-09 20:14:45 -0300

    Add CanCanCan completo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, 
+    before_action :configure_permitted_parameters, 
 
-  if: :devise_controller?
+    if: :devise_controller?
 
-    protected
+      protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << [:name, :address]
-      devise_parameter_sanitizer.for(:account_update) << [:name, :address]
-    end
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) << [:name, :address]
+        devise_parameter_sanitizer.for(:account_update) << [:name, :address]
+      end
 
+      rescue_from CanCan::AccessDenied do |exception|
+        redirect_to root_url, :alert => exception.message
+      end
 
 
 end
